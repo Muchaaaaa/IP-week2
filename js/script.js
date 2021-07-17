2b5f6e27a06f0f9db5d1b713810d6a17b9dac7f0
Muchaaaaa <sylviaslyg@gmail.com>
2021-07-17 19:32:01 +0300

    linking javascript file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-function{
+function onsubmit(birthday,gender){
+    let CC="century";
+    let YY="Year";
+    let MM="Month";
+    let DD="day of month";
+    let mod="Modulus function";
+console.log("You Akan Name is "+Akan Name);
+    
+    }
 
 
     
